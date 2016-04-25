764e6e354ab3b78bc90796530e8704c335bb3bc0
amitoo7gun <amitsrivastava2050@rediffmail.com>
2016-04-26 00:00:34 +0530

    feat: Added paid version Paid version configured having features different from free version of application.

--- myjokeandroidlibrary/src/main/java/com/example/myjokeandroidlibrary/MainJokeActivity.java
+++ myjokeandroidlibrary/src/main/java/com/example/myjokeandroidlibrary/MainJokeActivity.java
@@ -1,16 +1,23 @@
 package com.example.myjokeandroidlibrary;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class MainJokeActivity extends AppCompatActivity {
 
+    private ProgressBar spinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_joke);
+
+        spinner=(ProgressBar)findViewById(R.id.progressBar);
+        spinner.setVisibility(View.VISIBLE);
         TextView jokeTextView = (TextView)findViewById(R.id.jokeText);
         jokeTextView.setText(getIntent().getStringExtra("JOKE"));
+        spinner.setVisibility(View.GONE);
     }
 }
